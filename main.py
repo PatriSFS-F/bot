6a5a6859fcfcb03360aa67d9641442fb676ae6be
PatriSFS-F <patricksfs.silva@gmail.com>
2024-09-07 10:58:44 -0300

    teste para evitar coisas

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import webserver
 import logging
 
 # Configuração do bot com intents ativados
-
 intents = discord.Intents.default()
 intents.presences = True 
 intents.messages = True  
@@ -15,7 +14,6 @@ intents.members = True
 intents.dm_messages = True 
 
 # Configurações do bot
-
 TOKEN = os.environ.get('discordkey')
 ROLE_NAME = '📺⠂Ao vivo' 
 STREAMER_ROLE_NAME = 'Streamer' 
@@ -23,7 +21,6 @@ KEYWORDS = ['Code', 'CODE', 'code', 'CodeRp','[CodeRp]']
 ALLOWED_GUILD_ID = 1249889579041820823
 
 # dev e Gaucheira
-
 AUTHORIZED_USERS = [757934740308361247, 691679550140055573]
 TARGET_CHANNEL_ID = 1257959837866786868
 
@@ -50,9 +47,7 @@ def contains_keyword(activity_name):
 # permite mandar mensagem em um canal especifico
 @bot.event
 async def on_message(message):
-    # Verifica se a mensagem foi enviada em um DM e se o autor não é um bot
     if isinstance(message.channel, discord.DMChannel) and not message.author.bot:
-        # Verifica se o autor está na lista de usuários autorizados
         if message.author.id in AUTHORIZED_USERS:
             target_channel = bot.get_channel(TARGET_CHANNEL_ID)
             if target_channel:
@@ -63,75 +58,58 @@ async def on_message(message):
         else:
             logger.warning(f'{message.author} tentou enviar uma mensagem, mas não está autorizado.')
     
-    # Permite que outros comandos sejam processados
     await bot.process_commands(message)
 
 @bot.command(name='teste')
 async def teste(ctx):
     logger.info(f'Comando "teste" chamado por {ctx.author.id}.')
     if ctx.author.id in AUTHORIZED_USERS:
-        logger.info(f'{ctx.author.display_name} está autorizado a usar o comando teste.')
         await ctx.send(f"{ctx.author.display_name} está ao vivo! Assista em https://www.youtube.com/watch?v=KYimxdQlogg")
     else:
         await ctx.send("Você não tem permissão para usar este comando.")
 
-
-# Função para dar cargo a streamer quando tiverem ao vivo e no CodeRP
+# Função para gerenciar cargos de streaming ao vivo
 @bot.event
 async def on_presence_update(before, after):
     guild = after.guild
     
-    # Verifica se o usuário começou ou parou de fazer live
-    if before.activities != after.activities:
-        role = discord.utils.get(guild.roles, name=ROLE_NAME)
-        streamer_role = discord.utils.get(guild.roles, name=STREAMER_ROLE_NAME)
+    if before.activities == after.activities:  # Sem mudança nas atividades
+        return
 
-        # Checa se o usuário tem o cargo "Streamer" e está transmitindo ao vivo
-        if role and streamer_role and streamer_role in after.roles:
-            # Verifica se a atividade é um streaming e contém qualquer palavra-chave
-            if any(activity.type == discord.ActivityType.streaming and contains_keyword(activity.name) for activity in after.activities):
-                # Atribui o cargo se estiver em live com a palavra-chave no nome
-                if role not in after.roles:
-                    try:
-                        await after.add_roles(role)
+    role = discord.utils.get(guild.roles, name=ROLE_NAME)
+    streamer_role = discord.utils.get(guild.roles, name=STREAMER_ROLE_NAME)
 
-                        print(f'Cargo "{ROLE_NAME}" atribuído a {after.display_name}')
-                        
-                        # Envia a mensagem para o canal específico
-                        target_channel = bot.get_channel(TARGET_CHANNEL_ID)
-                        if target_channel:
-                            # Encontre a atividade de streaming com URL
-                            activity = next((a for a in after.activities if a.type == discord.ActivityType.streaming), None)
-                            if activity and activity.url:
-                                message = f"{after.display_name} está ao vivo! Assista em {activity.url}"
-                            else:
-                                message = f"{after.display_name} está ao vivo! Link não disponível"
-                            await target_channel.send(message)
-                            logger.info(f'Mensagem enviada para o canal {target_channel.name}: {message}')
-                        else:
-                            logger.error(f'Canal com ID {TARGET_CHANNEL_ID} não encontrado.')
-                        
-                    except discord.Forbidden:
-                        print(f'Permissões insuficientes para atribuir o cargo "{ROLE_NAME}" a {after.display_name}.')
-                    except discord.HTTPException as e:
-                        print(f'Ocorreu um erro ao atribuir o cargo: {e}')
-            else:
-                # Remove o cargo se não estiver mais em live ou se a atividade não contém a palavra-chave
-                if any(activity.type == discord.ActivityType.streaming for activity in before.activities) and not any(activity.type == discord.ActivityType.streaming and contains_keyword(activity.name) for activity in after.activities):
-                    if role in after.roles:
-                        try:
-                            await after.remove_roles(role)
-                            print(f'Cargo "{ROLE_NAME}" removido de {after.display_name}')
-                        except discord.Forbidden:
-                            print(f'Permissões insuficientes para remover o cargo "{ROLE_NAME}" de {after.display_name}.')
-                        except discord.HTTPException as e:
-                            print(f'Ocorreu um erro ao remover o cargo: {e}')
-        else:
-            print('checando atividade...')
+    if role and streamer_role and streamer_role in after.roles:
+        # Checa se o usuário está fazendo live com palavra-chave
+        streaming_activity = next((activity for activity in after.activities if activity.type == discord.ActivityType.streaming and contains_keyword(activity.name)), None)
 
-#  mantem ele ativo
+        if streaming_activity and role not in after.roles:
+            try:
+                await after.add_roles(role)
+                print(f'Cargo "{ROLE_NAME}" atribuído a {after.display_name}')
+                target_channel = bot.get_channel(TARGET_CHANNEL_ID)
+                if target_channel:
+                    message = f"{after.display_name} está ao vivo! Assista em {streaming_activity.url}" if streaming_activity.url else f"{after.display_name} está ao vivo! Link não disponível"
+                    await target_channel.send(message)
+                    logger.info(f'Mensagem enviada: {message}')
+                else:
+                    logger.error(f'Canal com ID {TARGET_CHANNEL_ID} não encontrado.')
+            except discord.Forbidden:
+                logger.error(f'Permissões insuficientes para atribuir o cargo "{ROLE_NAME}" a {after.display_name}.')
+            except discord.HTTPException as e:
+                logger.error(f'Erro ao atribuir o cargo: {e}')
+        elif not streaming_activity and role in after.roles:
+            try:
+                await after.remove_roles(role)
+                print(f'Cargo "{ROLE_NAME}" removido de {after.display_name}')
+            except discord.Forbidden:
+                logger.error(f'Permissões insuficientes para remover o cargo "{ROLE_NAME}" de {after.display_name}.')
+            except discord.HTTPException as e:
+                logger.error(f'Erro ao remover o cargo: {e}')
+    else:
+        print('Nenhuma ação necessária.')
+
+# Mantém o bot ativo
 webserver.keep_alive()
 # Inicializa o bot
 bot.run(TOKEN)
-
-
